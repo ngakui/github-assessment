185c3b7a726ab24ef0a51547d876e342e0cdc778
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-25 13:18:40 +0100

    Create function to get repositories from seach query.

--- src/app/features/repos/services/repos.service.ts
+++ src/app/features/repos/services/repos.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReposService {
 
-  constructor() { }
+  headers = {
+    'Authorization': 'Bearer '+ environment.access_token,
+    'X-GitHub-Api-Version': '2022-11-28',
+    'Accept': 'application/vnd.github+json'
+  }
+
+  constructor(private http: HttpClient) { }
+
+  searchRepos(query: string) {
+    return this.http.get(`${environment.apiUrl}?q=${query}`, { headers: this.headers }).pipe(
+      map((res: any) => res.items)
+    );
+  }
 }
