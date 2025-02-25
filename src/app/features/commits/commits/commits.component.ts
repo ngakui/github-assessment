63a76c75d064d071d88d0cd719094f280cc3dd98
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-25 20:30:38 +0100

    Generate commit component.

---
+++ src/app/features/commits/commits/commits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commits',
+  imports: [],
+  templateUrl: './commits.component.html',
+  styleUrl: './commits.component.scss'
+})
+export class CommitsComponent {
+
+}
