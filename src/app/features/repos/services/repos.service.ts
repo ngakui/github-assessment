c0f5dc77561e69daf5bddb8b202f0245b2fec96c
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-25 20:08:25 +0100

    Create different functions to search by name and by issue.

--- src/app/features/repos/services/repos.service.ts
+++ src/app/features/repos/services/repos.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { environment } from '../../../../environments/environment.development';
-import { map } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+import { forkJoin, map, switchMap } from 'rxjs';
+import { Repo } from '../model/repo.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,48 @@ import { map } from 'rxjs';
 export class ReposService {
 
   private headers = {
-    // 'Authorization': 'Bearer '+ environment.access_token,
+    'Authorization': 'Bearer '+ environment.access_token,
     'X-GitHub-Api-Version': '2022-11-28',
     'Accept': 'application/vnd.github+json'
   }
 
   constructor(private http: HttpClient) { }
 
-  // searchRepos(query: string) {
-  //   return this.http.get(`${environment.apiUrl}?q=${query}`, { headers: this.headers }).pipe(
-  //     map((res: any) => res.items)
-  //   );
-  // }
+  searchRepos(query: string, searchBy: string, language: string, minStars: number) {
+    if(searchBy === 'name')
+      return this.searchRepoByName(query, language, minStars);
+    else
+      return this.searchRepoByIssue(query, language);
+  }
+
+  searchRepoByName(repoName: string, language: string, minStars: number) {
+    let searchQuery = `${repoName}`;
+    if(language)
+      searchQuery += `+language:${language}`;
+    if(minStars)
+      searchQuery += `+stars:>${minStars}`;
+    return this.http.get(`${environment.apiUrl}/repositories?q=${searchQuery}`).pipe(
+      map((res: any) => <Repo[]>res.items)
+    );
+  }
+
+  searchRepoByIssue(issueTitle: string, language: string) {
+    let searchQuery = `${issueTitle}+is:issue`;
+    if(language)
+      searchQuery += `+language:${language}`;
+    return this.http.get(`${environment.apiUrl}/issues?q=${searchQuery}`).pipe(
+      // Extract the repository URLs
+      map((res: any) => {
+        const repoUrls = new Set<string>(res.items.map((item: any) => item.repository_url));
+        return Array.from(repoUrls);
+      }),
+      // Fetch the repositories
+      switchMap((repoUrls: string[]) => {
+        const repoRequests = repoUrls.map((url: string) => this.http.get(url));
+        return forkJoin(repoRequests);
+      }),
+      // Map the response to the Repo model
+      map((res: any) => <Repo[]>res )
+    );
+  }
 }
