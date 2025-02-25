7e3d32dad6353bfaaf44f135ce7acc9632df0d03
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-25 13:22:03 +0100

    Generate repos component.

---
+++ src/app/features/repos/repos/repos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repos',
+  imports: [],
+  templateUrl: './repos.component.html',
+  styleUrl: './repos.component.scss'
+})
+export class ReposComponent {
+
+}
