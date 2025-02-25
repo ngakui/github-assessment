bb9718e8db760c825e2bcc69c45cae419353fa33
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-25 20:48:08 +0100

    Add function to get Commits.

--- src/app/features/commits/services/commits.service.ts
+++ src/app/features/commits/services/commits.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Commit } from '../model/commit.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommitsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Fetch commits for a given repository and specifc user
+  getCommits(repoName: string, userName: string): Observable<Commit[]> {
+    return this.http.get<Commit[]>(`${environment.github_api}/repos/${userName}/${repoName}/commits`).pipe(
+      map((res: any) => <Commit[]>res)
+    );
+  }
 }
