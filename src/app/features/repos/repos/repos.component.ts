f3657b7af98b43b85a7c957d06a8b72412701d2d
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-25 15:55:50 +0100

    Create a search function and update the html.

--- src/app/features/repos/repos/repos.component.ts
+++ src/app/features/repos/repos/repos.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, signal, WritableSignal } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { ReposService } from '../services/repos.service';
+import { Router } from '@angular/router';
+import { Repo } from '../model/repo.model';
+import { Subscription } from 'rxjs';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-repos',
-  imports: [],
+  imports: [MatTableModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatSortModule,
+    FormsModule,
+    DatePipe
+  ],
   templateUrl: './repos.component.html',
   styleUrl: './repos.component.scss'
 })
-export class ReposComponent {
+export class ReposComponent implements OnDestroy {
 
+  searchQuery: string = '';
+  repos: WritableSignal<Repo[]> = signal([]);
+
+  repoSubscriptions: Subscription[] = [];
+
+  constructor(private repoService: ReposService, private router: Router) { }
+
+  search() {
+    this.repoSubscriptions.push(this.repoService.searchRepos(this.searchQuery).subscribe((res: Repo[]) => {
+      this.repos.set(res);
+    }));
+  }
+
+  viewCommits(repoName: string) {
+    this.router.navigate(['/commits', repoName]);
+  }
+
+  ngOnDestroy() {
+    this.repoSubscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
