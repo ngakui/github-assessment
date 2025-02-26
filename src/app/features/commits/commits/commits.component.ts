4d083a56ad9a806b689001e3f2ae7ebbf45e44b3
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-26 10:16:07 +0100

    Add reactive form.

--- src/app/features/commits/commits/commits.component.ts
+++ src/app/features/commits/commits/commits.component.ts
@@ -1,12 +1,28 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
 import { CommitsService } from '../services/commits.service';
 import { Commit } from '../model/commit.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-commits',
-  imports: [MatTableModule],
+  imports: [
+    MatTableModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule, 
+    MatButtonModule,
+    CommonModule, 
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './commits.component.html',
   styleUrl: './commits.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -15,22 +31,45 @@ export class CommitsComponent implements OnDestroy, OnInit {
 
   commits: WritableSignal<Commit[]> = signal([]);
   repoName: WritableSignal<string> = signal('');
+  repoOwner: WritableSignal<string> = signal('');
 
-  constructor(private commitService: CommitsService, private route: ActivatedRoute) {
-    this.route.params.subscribe(params => {
+  searchForm: FormGroup = new FormGroup({});
+
+  //unsubscribe from all subscriptions
+  commitSubscriptions: Subscription[] = [];
+
+  constructor(private commitService: CommitsService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      searchQuery: ['', Validators.required]
+    });
+    this.commitSubscriptions.push(this.route.params.subscribe(params => {
       console.log('params', params);
       this.repoName.set(params['repo']);
-      this.commitService.getReposCommits(params['repo'], params['owner']).subscribe((res: Commit[]) => {
+      this.repoOwner.set(params['owner']);
+      this.commitService.getReposCommits(this.repoName(), this.repoOwner()).subscribe((res: Commit[]) => {
         this.commits.set(res);
       });
-    });
-   }
+    }));
+  }
+
+  searchCommits() {
+    if(this.searchForm.invalid && this.searchForm.get('searchQuery')?.value.trim() === '')
+      return;
+    this.commitSubscriptions.push(this.commitService.searchRepoCommits(this.searchForm.get('searchQuery')?.value, this.repoName(), this.repoOwner()).subscribe((res: Commit[]) => {
+      this.commits.set(res);
+    }));
+  }
+
+  backToRepos() {
+    this.router.navigate(['/repos']);
+  }
 
   ngOnInit() {
   
   }
 
   ngOnDestroy() {
+    this.commitSubscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
