96741afe98b13553cb715d9d495ec707af00e836
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-02-26 08:41:48 +0100

    Get repos commits base on route params.

--- src/app/features/commits/commits/commits.component.ts
+++ src/app/features/commits/commits/commits.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { CommitsService } from '../services/commits.service';
+import { Commit } from '../model/commit.model';
+import { ActivatedRoute } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-commits',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './commits.component.html',
-  styleUrl: './commits.component.scss'
+  styleUrl: './commits.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CommitsComponent {
+export class CommitsComponent implements OnDestroy, OnInit {
+
+  commits: WritableSignal<Commit[]> = signal([]);
+  repoName: WritableSignal<string> = signal('');
+
+  constructor(private commitService: CommitsService, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      console.log('params', params);
+      this.repoName.set(params['repo']);
+      this.commitService.getReposCommits(params['repo'], params['owner']).subscribe((res: Commit[]) => {
+        this.commits.set(res);
+      });
+    });
+   }
+
+  ngOnInit() {
+  
+  }
+
+  ngOnDestroy() {
+  }
 
 }
